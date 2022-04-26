ca34f0e5f5edd733c29e3b21d95a7d1cff6ce003
Pranav <pranavmasekar4@gmail.com>
2022-04-26 23:38:02 +0530

    Tree Questions

---
+++ Trees/balancedBST.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root==NULL) return 0;
+        return 1 + max(maxDepth(root->left) , maxDepth(root->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
+        if(root->left==NULL && root->right==NULL) return true;
+        if(abs(maxDepth(root->left) - maxDepth(root->right))>1) return false;
+        else{
+            return isBalanced(root->left) && isBalanced(root->right);
+        }
+
+    }
+};
+
+int main(){
+    
+    return 0;
+}
