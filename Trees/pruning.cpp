8d62d5be97c16085c697fa3c8bc50c61788cd719
Pranav <pranavmasekar4@gmail.com>
2022-05-15 17:29:32 +0530

    pruning

--- Trees/pruning.cpp
+++ Trees/pruning.cpp
@@ -10,43 +10,21 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-// Incomplete
 class Solution {
-    TreeNode* newRoot;
-    TreeNode* temp;
     bool isValid(TreeNode* root){
         if(root==NULL) return false;
         if(root->val==1) return true;
         else return isValid(root->left) || isValid(root->right);
     }
-    void solve(TreeNode* root,TreeNode* myRoot){
-        if(root==NULL) return;
-        if(root->left!=NULL && !isValid(root->left)){
-            myRoot->left = NULL;
-        }
-        else {
-            TreeNode* newNode = new TreeNode(root->val);
-            myRoot->left = newNode;
-        }
-       
-        if(root->right!=NULL && !isValid(root->right)){
-            myRoot->right = NULL;
-        }
-        else {
-            TreeNode* newNode = new TreeNode(root->val);
-            myRoot->right = newNode;
-        }
-        solve(root->left,myRoot->left);
-        solve(root->right,myRoot->right);
-    }
 public:
     TreeNode* pruneTree(TreeNode* root) {
         if(root==NULL) return root;
-        TreeNode* newNode = new TreeNode(root->val);
-        newRoot = newNode;
-        temp = root;
-        solve(root,temp);
-        return newRoot;
+        if(isValid(root)){
+            root->left = pruneTree(root->left);
+            root->right = pruneTree(root->right);
+            return root;
+        }
+        else return NULL;
     }
 };
 
